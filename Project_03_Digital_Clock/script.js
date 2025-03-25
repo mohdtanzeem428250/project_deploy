1cc75b2f0fd3983709921ae4cf1974d4076204de
Mohd Tanzeem <mohdtanzeem169@gmail.com>
2025-03-25 19:09:24 +0530

    Update script.js

--- Project_03_Digital_Clock/script.js
+++ Project_03_Digital_Clock/script.js
@@ -1,21 +1,6 @@
-const form=document.querySelector("form");
-form.addEventListener("submit",function(e)
+const clock=document.getElementById("clock");
+setInterval(()=>
 {
-    e.preventDefault();
-    const height=(document.querySelector("#height").value);
-    const weight=(document.querySelector("#weight").value);
-    const result=document.querySelector("#result");
-    if(height===''||height<=0||isNaN(height))
-    {
-        result.innerText=`Please Give A Valid Height (${height})`;
-    }
-    else if(weight===''||weight<=0||isNaN(weight))
-    {
-        result.innerText=`Please Give A Valid Weight (${weight})`;
-    }
-    else
-    {
-        const bmiCalculate=(weight/((height*height)/10000)).toFixed(2);
-        result.innerText=`${bmiCalculate}`;
-    }
-})
+    let date=new Date();
+    clock.innerText=date.toLocaleTimeString();
+},1000)
