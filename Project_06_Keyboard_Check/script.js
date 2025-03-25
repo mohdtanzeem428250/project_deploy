85e103ed25200ccc3a14c728bd6662a8245f756b
Mohd Tanzeem <mohdtanzeem169@gmail.com>
2025-03-25 19:46:26 +0530

    Add files via upload

---
+++ Project_06_Keyboard_Check/script.js
@@ -0,0 +1,22 @@
+const insert=document.getElementById("insert");
+// console.log(insert);
+window.addEventListener('keydown',function(e)
+{
+    insert.innerHTML=
+    `
+    <div class="color">
+    <table>
+        <tr>
+            <th>Key</th>
+            <th>Key Code</th>
+            <th>Code</th>
+        </tr>
+        <tr>
+            <td>${e.key===" "?"Space":e.key}</td>
+            <td>${e.keyCode}</td>
+            <td>${e.code}</td>
+        </tr>
+    </table>
+    </div>
+    `;
+})
